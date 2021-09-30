342c07dc7f9cf4b9ed9510aea8e2cfda24869983
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-09-30 01:08:05 -0300

    Validações avançadas de formulário

--- Formularios/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ Formularios/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,31 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 import { MASKS, NgBrazilValidators } from 'ng-brazil';
 import { Usuario } from './models/Usuario';
-import {utilsBr} from 'js-brasil';
+import { utilsBr } from 'js-brasil';
 import { CustomValidators } from 'ng2-validation';
+import { DisplayMessage, GenericValidator, ValidationMessages } from './generic-form-validation';
+import { fromEvent, merge, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html'
 })
-export class CadastroComponent implements OnInit {
+export class CadastroComponent implements OnInit,AfterViewInit {
+
+  @ViewChildren(FormControlName,{read: Element}) formInputElements : ElementRef[]; // trabalhando com a view seleciona colecao de itens do formulario
 
   cadastroForm: FormGroup;
   usuario: Usuario;
   formResult: string = '';
   Masks = utilsBr.MASKS;
-  constructor(private fb: FormBuilder) { }
+
+  validationMessages: ValidationMessages;
+  genericValidator: GenericValidator;
+  displayMessage: DisplayMessage = {};
+
+  constructor(private fb: FormBuilder) {
+
+    this.validationMessages = {
+      nome: {
+        required: 'O nome e requerido',
+        minlength: 'O nome precisa ter no minino 2 caracteres',
+        maxlength: 'O nome precisa ter no maximo 150 caracteres'
+      },
+      cpf: {
+        required: 'Informe o CPF',
+        cpf: 'CPF em formato inválido'
+      },
+      email: {
+        required: 'Informe o e-mail',
+        email: 'Email inválido'
+      },
+      senha: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      senhaConfirmacao: {
+        required: 'Informe a senha novamente',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      }
+    };
+
+    this.genericValidator = new GenericValidator(this.validationMessages);
+  }
+  ngAfterViewInit(): void {
+
+    let controlBlurs :Observable<any>[] = this.formInputElements
+    .map((formControl : ElementRef) => fromEvent(formControl.nativeElement,'blur'));
+
+    merge(...controlBlurs).subscribe(()=>{
+      this.displayMessage = this.genericValidator.processarMensagens(this.cadastroForm);
+    });
+
+  } // apos carregar o Html
 
   ngOnInit() {
 
-    let senha = new FormControl('',[Validators.required,CustomValidators.rangeLength([6,15])]);
-    let senhaConfirm = new FormControl('',[Validators.required,CustomValidators.rangeLength([6,15]),CustomValidators.equalTo(senha)]);
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15])]);
+    let senhaConfirm = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15]), CustomValidators.equalTo(senha)]);
 
     this.cadastroForm = this.fb.group({
-      nome: ['', Validators.required,Validators.minLength(2),Validators.maxLength(150)],
-      cpf: ['',Validators.required,NgBrazilValidators.cpf],
-      email: ['',[Validators.required,Validators.email]],
+      nome: ['', Validators.required, Validators.minLength(2), Validators.maxLength(150)],
+      cpf: ['', Validators.required, NgBrazilValidators.cpf],
+      email: ['', [Validators.required, Validators.email]],
       senha: senha,
       senhaConfirmacao: senhaConfirm
     });
@@ -36,8 +83,8 @@ export class CadastroComponent implements OnInit {
       this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
       this.formResult = JSON.stringify(this.cadastroForm.value);
     }
-    else{
-      this.formResult ="Nao Submeteu!!!";
+    else {
+      this.formResult = "Nao Submeteu!!!";
     }
   }
 
