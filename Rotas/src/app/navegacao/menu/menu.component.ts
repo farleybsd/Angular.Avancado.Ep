57cb029f08c2dca7f2497d576afbb33f2d36cf8a
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-10-07 11:35:45 -0300

    Links de navegação dinâmicos

--- Rotas/src/app/navegacao/menu/menu.component.ts
+++ Rotas/src/app/navegacao/menu/menu.component.ts
@@ -4,4 +4,38 @@ import { Component } from '@angular/core';
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent {}
+export class MenuComponent {
+  nav : Nav[] =[
+    {
+      link:'/home',
+      name: 'Home',
+      exact:true,
+      admim:false
+    },
+    {
+      link:'/Cadastro',
+      name: 'Cadastro',
+      exact:true,
+      admim:false
+    },
+    {
+      link:'/sobre',
+      name: 'Sobre',
+      exact:true,
+      admim:false
+    },
+    {
+      link:'/produto',
+      name: 'Produto',
+      exact:true,
+      admim:false
+    }
+  ]
+}
+
+interface Nav{
+  link:string,
+  name: string,
+  exact: boolean,
+  admim: boolean
+}
