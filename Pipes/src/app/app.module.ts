2af1577f586288ddf53c47cb85d9c22d8cd0a6a9
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-10-17 00:28:09 -0300

    Aplicando Pipes no Angular

---
+++ Pipes/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+
+import { NgBrazil } from 'ng-brazil'
+import { TextMask } from 'ng-brazil';
+import { CustomFormsModule } from 'ng2-validation'
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
+import { AppComponent } from './app.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
+
+import { AppRoutingModule } from './app.routes';
+import { ProdutoAppComponet } from './demos/arquitetura-componentes/produto.app.component';
+import { AuthGuard } from './Services/app.guard';
+import { CadastroGuard } from './Services/cadastro.guard';
+import { FilmesComponent } from './demos/pipes/filmes/FilmesComponent';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SobreComponent,
+    CadastroComponent,
+    ProdutoAppComponet,
+    FilmesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NavegacaoModule,
+    TextMask.TextMaskModule,
+    NgBrazil,
+    CustomFormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+   // {provide: APP_BASE_HREF, useValue: '/'} adc no index.htmL
+   AuthGuard,
+   CadastroGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Pipes/src/app/navegacao/menu/menu.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent {
+  nav : Nav[] =[
+    {
+      link:'/home',
+      name: 'Home',
+      exact:true,
+      admim:false
+    },
+    {
+      link:'/cadastro',
+      name: 'Cadastro',
+      exact:true,
+      admim:false
+    },
+    {
+      link:'/sobre',
+      name: 'Sobre',
+      exact:true,
+      admim:false
+    },
+    {
+      link:'/produtos',
+      name: 'Produto',
+      exact:false,
+      admim:false
+    },
+    {
+      link: '/filmes',
+      name: 'Filmes',
+      exact: false,
+      admim: false
+    },
+    {
+      link: '/admin',
+      name: 'Admin',
+      exact: false,
+      admim: false
+    }
+  ]
+}
+
+interface Nav{
+  link:string,
+  name: string,
+  exact: boolean,
+  admim: boolean
+}
