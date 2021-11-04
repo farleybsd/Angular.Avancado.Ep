d0b03f68f580ed02b2a8579bd4576c0949db3b52
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-11-04 13:23:06 -0300

    Testes em componentes

--- testesAngular/Pipes/src/app/demos/contador/contator.component.spec.ts
+++ testesAngular/Pipes/src/app/demos/contador/contator.component.spec.ts
@@ -1,49 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContadorComponent } from './contator.component';
 
-describe('ContatorComponent', () => {
+describe('ContadorComponent', () => {
 
     let component: ContadorComponent;
     let fixture: ComponentFixture<ContadorComponent>;
 
+    // Setup
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [
                 ContadorComponent
             ]
-        });
+        })
 
-        fixture = TestBed.createComponent(ContadorComponent);
+        fixture = TestBed.createComponent(ContadorComponent)
         component = fixture.componentInstance;
-
         component.valor = 0;
     });
 
     it('Deve incrementar corretamente', () => {
         component.incrementar()
-        expect(component.valor).toBe(1);
+        expect(component.valor).toBe(1)
     });
 
-    it('Deve decrementar corretamente', () => {
+    it('Deve decrementar corretamemte', () => {
+
         component.incrementar()
-        expect(component.valor).toBe(1);
+        expect(component.valor).toBe(1)
+
         component.decrementar()
-        expect(component.valor).toBe(0);
+        expect(component.valor).toBe(0)
     });
 
-    it('não deve decrementar abaixo do valor permitido', () => {
+    it('Nao deve decrementar abaixo do valor permitido', () => {
+
         component.incrementar()
-        expect(component.valor).toBe(1);
+        expect(component.valor).toBe(1)
+
         component.decrementar()
-        expect(component.valor).toBe(0);
+        expect(component.valor).toBe(0)
+
         component.decrementar()
-        expect(component.valor).toBe(0);
+        expect(component.valor).toBe(0)
     });
 
-    it('não deve incrementar acima do valor permitido', () => {
+    it('Não deve incrementar acima do valor permitido', () => {
+
         for (let i = 0; i < 200; i++) {
-            component.incrementar();
+            component.incrementar()
         }
-        expect(component.valor).toBe(100);
+        expect(component.valor).toBe(100)
     });
-});
+})
