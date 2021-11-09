38374e6161a97a015eab6b74f404084d8699bf7a
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-11-09 01:20:26 -0300

    Estrutura do módulo de autenticação

---
+++ ProjetoFinal/front-end/src/app/conta/conta.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { ContaAppComponent } from './conta.app.component';
+
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http"
+
+import { ContaRoutingModule } from './conta.route';
+
+
+@NgModule({
+  declarations: [
+    ContaAppComponent,
+    CadastroComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ContaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ]
+})
+export class ContaModule { }
