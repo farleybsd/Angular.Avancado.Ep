88ec81dbac6aaa3441b011c1796f98e8f4c39c25
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-11-18 00:34:55 -0300

    Notificações com Toastr

--- ProjetoFinal/front-end/src/app/app.module.ts
+++ ProjetoFinal/front-end/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegacaoModule } from './navegacao/navegacao.module';
 
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { NavegacaoModule } from './navegacao/navegacao.module';
     BrowserModule,
     AppRoutingModule,
     NavegacaoModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProjetoFinal/front-end/src/app/conta/cadastro/cadastro.component.ts
+++ ProjetoFinal/front-end/src/app/conta/cadastro/cadastro.component.ts
@@ -9,13 +9,14 @@ import { DisplayMessage, GenericValidator, ValidationMessages } from 'src/app/ut
 import { Usuario } from '../models/usuario';
 import { ContaService } from '../services/conta.service';
 
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit, AfterViewInit {
-  
+
   @ViewChildren(FormControlName, { read: ElementRef }) formInputElements!: ElementRef[];
 
   errors: any[] = [];
@@ -29,33 +30,34 @@ export class CadastroComponent implements OnInit, AfterViewInit {
   constructor(
     private fb: FormBuilder,
     private contaService: ContaService,
-    private router : Router
-    ) {
-      this.validationMessages = {
-        email: {
-          required: 'Informe o e-mail',
-          email: 'Email inválido'
-        },
-        password: {
-          required: 'Informe a senha',
-          rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
-        },
-        confirmPassword: {
-          required: 'Informe a senha novamente',
-          rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
-          equalTo: 'As senhas não conferem'
-        }
-      };
-      this.genericValidator = new GenericValidator(this.validationMessages)
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    this.validationMessages = {
+      email: {
+        required: 'Informe o e-mail',
+        email: 'Email inválido'
+      },
+      password: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      confirmPassword: {
+        required: 'Informe a senha novamente',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      }
+    };
+    this.genericValidator = new GenericValidator(this.validationMessages)
   }
 
   ngOnInit(): void {
-    
+
     let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15])]);
     let senhaConfirm = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15]), CustomValidators.equalTo(senha)]);
 
     this.cadastroForm = this.fb.group({
-      email: ['',[Validators.required,Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       password: senha,
       confirmPassword: senhaConfirm
     })
@@ -63,34 +65,42 @@ export class CadastroComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     let controlBlurs: Observable<any>[] = this.formInputElements
-    .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
 
-  merge(...controlBlurs).subscribe(() => {
-    this.displayMessage = this.genericValidator.processarMensagens(this.cadastroForm);
-    
-  });
+    merge(...controlBlurs).subscribe(() => {
+      this.displayMessage = this.genericValidator.processarMensagens(this.cadastroForm);
+
+    });
   }
 
   AdicionarConta() {
     if (this.cadastroForm.dirty && this.cadastroForm.valid) {
       this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value)
       this.contaService.registrarUsuario(this.usuario).subscribe(
-        sucesso => {this.processarSucesso(sucesso)},
-        falha => {this.processarFalha(falha)}
+        sucesso => { this.processarSucesso(sucesso) },
+        falha => { this.processarFalha(falha) }
       );
     }
 
   }
 
-  processarSucesso(response : any) : void {
+  processarSucesso(response: any): void {
     this.cadastroForm.reset();
-    this.errors =[];
+    this.errors = [];
     this.contaService.localStorage.salvarDadosLocaisUsuario(response);
-    this.router.navigate(['/home'])
+    let toast = this.toastr.success('Registro Realizado Com Sucesso!', 'Seja Bem Vindo :)')
+
+    if (toast) {
+      toast.onHidden.subscribe(() => {
+        this.router.navigate(['/home'])
+      })
+    }
+
   }
 
-  processarFalha(fail:any){
+  processarFalha(fail: any) {
     this.errors = fail.error.errors;
+    this.toastr.error('Ocorreu um erro!', 'Opa :(')
   }
 
 }
